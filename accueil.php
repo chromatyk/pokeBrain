6a732f3c4fac0c3bd76057297d2d2892c46c2f73
Lexae <atparky@outlook.fr>
2018-11-14 14:21:57 +0100

    bite2

---
+++ accueil.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" type="image/png" href="assets/images/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-grid.min.css" />
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <title>PokeBrain - Connexion</title>
+    </head>
+    <body>
+        <h1>TEST</h1>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
