452f28be97e9904a0fe62ca8d46d509bd39a0842
chromatyk <42806308+chromatyk@users.noreply.github.com>
2018-11-16 11:54:05 +0100

    Header / menu

---
+++ vues/header.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" type="image/png" href="assets/images/favicon.ico" />
+        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+        
+        <link rel="stylesheet" type="text/css" href="assets/css/home.css" />
+        <link rel="stylesheet" type="text/css" href="../assets/css/fontAwesome.css" />
+        <link rel="stylesheet" type="text/css" href="../assets/css/header.css" />
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+        <title>PokeBrain</title>
+    </head>
+    <body>
+		<div class="container">
+			<div class="menu-wrap">
+				<nav class="menu-top">
+					<div class="profile"><img src="img/user1.png" alt="Matthew Greenberg"/><span>Matthew Greenberg</span></div>
+					<div class="icon-list">
+						<a href="#"><i class="fa fa-fw fa-star-o"></i></a>
+						<a href="#"><i class="fa fa-fw fa-bell-o"></i></a>
+						<a href="#"><i class="fa fa-fw fa-envelope-o"></i></a>
+						<a href="#"><i class="fa fa-fw fa-comment-o"></i></a>
+					</div>
+				</nav>
+				<nav class="menu-side">
+					<a href="#">Recent Stories</a>
+					<a href="#">Reading List</a>
+					<a href="#">My Stories</a>
+					<a href="#">Categories</a>
+				</nav>
+			</div>
+			<button class="menu-button" id="open-button">Open Menu</button>
+			<div class="content-wrap">
+				<div class="content">
+						<nav class="codrops-demos">
+						</nav>
+                                    <main class="codrops-main">
+						<nav class="codrops-demos">
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+                                                    <p>o</p>
+						</nav>
+                                        </main>
+				</div>
+			</div><!-- /content-wrap -->
+		</div><!-- /container -->
+                <script src="../assets/js/offCanvasMenu.js"></script>
+	</body>
+            
