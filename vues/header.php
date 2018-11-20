59807ee9f7f72f682c70dd4fe5ecbf30ea5bcd29
chromatyk <42806308+chromatyk@users.noreply.github.com>
2018-11-20 12:16:06 +0100

    tamere

--- vues/header.php
+++ vues/header.php
@@ -4,7 +4,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<<<<<<< HEAD
         <link rel="icon" type="image/png" href="/assets/images/favicon.ico" />
         <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="/assets/css/home.css" />
@@ -15,16 +14,6 @@
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-=======
-        <link rel="icon" type="image/png" href="assets/images/favicon.ico" />
-        <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="assets/css/home.css" />
-        <link rel="stylesheet" type="text/css" href="assets/css/fontAwesome.css" />
-        <link rel="stylesheet" type="text/css" href="assets/css/header.css" />
-        <link rel="stylesheet" type="text/css" href="assets/css/profile.css" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
->>>>>>> c20fc0be485d899735b187ce6551bebd2152bc83
         <title>PokeBrain</title>
     </head>
     <body>
