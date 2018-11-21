2d78e8340b8e79f056bc92fd65cfc53b09c51791
chromatyk <42806308+chromatyk@users.noreply.github.com>
2018-11-21 15:54:45 +0100

    profil v3

--- profile.php
+++ profile.php
@@ -1,10 +1,5 @@
 <?php
 include 'vues/header.php';
-include_once 'models/dataBase.php';
-include_once 'models/users.php';
-include_once 'models/safaryFriend.php';
-include_once 'models/hunts.php';
-include_once 'controllers/usersInfos.php';
 ?>
 <div class="cardProfile">
     <div class="profilePicture"><img src="/assets/images/Lexae.png"></div>
@@ -16,26 +11,27 @@ include_once 'controllers/usersInfos.php';
         <div id="biouser"><?= $userConnected->bioUsers ?></div>
     </div>
 </div>
-    <div class="tile" data-scale="1.1" data-image="/assets/images/banniere.png"></div>
-<div class="safariPark offset-lg-3 col-lg-8">
+<div class="tile" data-scale="1.05" data-image="/assets/images/banniere.png"></div>
+<div class="safariPark col-xs-12 offset-lg-3 col-lg-8">
     <div class="col-sm-offset-5 col-md-offset-2 col-lg-offset-4">
-        <p id="caProfil">Code Ami : <?= $userConnected->friendCode; ?></p>
+        <center><p id="caProfil">Code Ami : <?= $userConnected->friendCode; ?></p></center>
     </div>
     <?php
     if ($safaryFriend) {
         ?>
-        <div id = "safariHub">
-            <div class = "col-xs-10 col-sm-11 col-md-9 col-lg-10">
-                <p id = "friendPark" >Safari Ami <img id = "typePark" src = "/assets/spriteSafari/<?= $safaryFriend->type ?>.png"/></p>
-            </div>
-            <div class = "col-xs-offset-2 col-xs-10 col-sm-offset-0 col-sm-10 col-md-offset-0 col-md-9 col-lg-offset-0 col-lg-10">
-                <span id = "pkmPark">
-                    <img src = "/assets/spriteSafari/<?= $safaryFriend->firstPokemon ?>.png"/>
-                    <img src = "/assets/spriteSafari/<?= $safaryFriend->secondPokemon ?>.png"/>
-                    <img src = "/assets/spriteSafari/<?= $safaryFriend->thirdPokemon ?>.png"/>
-                </span>
-            </div>
-        </div>
+        <center><div id = "safariHub">
+                <div class = "col-xs-10 col-sm-11 col-md-9 col-lg-10">
+                    <p id = "friendPark" >Safari Ami </p>
+                    <center><img id = "typePark" src = "/assets/spriteSafari/<?= $safaryFriend->type ?>.png"/></center>
+                </div>
+                <div class = "col-xs-offset-2 col-xs-10 col-sm-offset-0 col-sm-10 col-md-offset-0 col-md-9 col-lg-offset-0 col-lg-10">
+                    <span id = "pkmPark">
+                        <img src = "/assets/spriteSafari/<?= $safaryFriend->firstPokemon ?>.png"/>
+                        <img src = "/assets/spriteSafari/<?= $safaryFriend->secondPokemon ?>.png"/>
+                        <img src = "/assets/spriteSafari/<?= $safaryFriend->thirdPokemon ?>.png"/>
+                    </span>
+                </div>
+            </div></center>
         <?php
     } else {
         ?>
@@ -54,43 +50,55 @@ include_once 'controllers/usersInfos.php';
         <div class="card">
             <div class="card-header" id="headingOne">
                 <h5 class="mb-0">
-                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <p class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                         Génération 1
-                    </button>
+                    </p>
                 </h5>
             </div>
 
             <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
                 <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <p class="progressHunt">151 / 151 - 100%</p>
+                    <div class="progress">
+                        <div class="progress-bar" style="width:100%"></div>
+                    </div>
+                    <center><a class="detailsHunt" href="#" title="Détails de la progression">Détails</a></center> 
                 </div>
             </div>
         </div>
         <div class="card">
             <div class="card-header" id="headingTwo">
                 <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <p class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                         Génération 2
-                    </button>
+                    </p>
                 </h5>
             </div>
             <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                 <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <p class="progressHunt">151 / 151 - 100%</p>
+                    <div class="progress">
+                        <div class="progress-bar" style="width:80%"></div>
+                    </div>
+                    <center><a class="detailsHunt" href="#" title="Détails de la progression">Détails</a></center>                 
                 </div>
             </div>
         </div>
         <div class="card">
             <div class="card-header" id="headingThree">
                 <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <p class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                         Génération 3
-                    </button>
+                    </p>
                 </h5>
             </div>
             <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
                 <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <p class="progressHunt">151 / 151 - 100%</p>
+                    <div class="progress">
+                        <div class="progress-bar" style="width:60%"></div>
+                    </div>
+                    <center><a class="detailsHunt" href="#" title="Détails de la progression">Détails</a></center>                 
                 </div>
             </div>
         </div>
@@ -98,14 +106,18 @@ include_once 'controllers/usersInfos.php';
         <div class="card">
             <div class="card-header" id="headingFour">
                 <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                    <p class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
                         Génération 4
-                    </button>
+                    </p>
                 </h5>
             </div>
             <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
                 <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <p class="progressHunt">151 / 151 - 100%</p>
+                    <div class="progress">
+                        <div class="progress-bar" style="width:40%"></div>
+                    </div>
+                    <center><a class="detailsHunt" href="#" title="Détails de la progression">Détails</a></center>                 
                 </div>
             </div>
         </div>
@@ -113,14 +125,18 @@ include_once 'controllers/usersInfos.php';
         <div class="card">
             <div class="card-header" id="headingFive">
                 <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                    <p class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
                         Génération 5
-                    </button>
+                    </p>
                 </h5>
             </div>
             <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
                 <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <p class="progressHunt">151 / 151 - 100%</p>
+                    <div class="progress">
+                        <div class="progress-bar" style="width:30%"></div>
+                    </div>
+                    <center><a class="detailsHunt" href="#" title="Détails de la progression">Détails</a></center>                 
                 </div>
             </div>
         </div>
@@ -128,14 +144,18 @@ include_once 'controllers/usersInfos.php';
         <div class="card">
             <div class="card-header" id="headingSix">
                 <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                    <p class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
                         Génération 6
-                    </button>
+                    </p>
                 </h5>
             </div>
             <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
                 <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <p class="progressHunt">151 / 151 - 100%</p>
+                    <div class="progress">
+                        <div class="progress-bar" style="width:20%"></div>
+                    </div>
+                    <center><a class="detailsHunt" href="#" title="Détails de la progression">Détails</a></center>                 
                 </div>
             </div>
         </div>
@@ -143,42 +163,24 @@ include_once 'controllers/usersInfos.php';
         <div class="card">
             <div class="card-header" id="headingSeven">
                 <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                    <p class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
                         Génération 7
-                    </button>
+                    </p>
                 </h5>
             </div>
             <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
                 <div class="card-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <p class="progressHunt">151 / 151 - 100%</p>
+                    <div class="progress">
+                        <div class="progress-bar" style="width:10%"></div>
+                    </div>
+                    <center><a class="detailsHunt" href="#" title="Détails de la progression">Détails</a></center>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <script src="/assets/js/parallax.js"></script>
-<script>
-$('.tile')
-    // tile mouse actions
-    .on('mouseover', function(){
-      $(this).children('.bannerParallax').css({'transform': 'scale('+ $(this).attr('data-scale') +')'});
-    })
-    .on('mouseout', function(){
-      $(this).children('.bannerParallax').css({'transform': 'scale(1)'});
-    })
-    .on('mousemove', function(e){
-      $(this).children('.bannerParallax').css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
-    })
-    // tiles set up
-    .each(function(){
-      $(this)
-        // add a photo container
-        .append('<div class="bannerParallax"></div>')
-        // some text just to show zoom level on current item in this example
-        // set up a background image for each tile based on data-image attribute
-        .children('.bannerParallax').css({'background-image': 'url('+ $(this).attr('data-image') +')'});
-    })
-</script>
 <?php
 include 'vues/footer.php';
 ?>
