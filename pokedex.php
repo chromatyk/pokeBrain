b7dca49b8ece924498245ca027b1829ef08d1d13
Lexae <atparky@outlook.fr>
2018-11-21 10:49:30 +0100

    Création du pokedex

---
+++ pokedex.php
@@ -0,0 +1,14 @@
+<?php
+include 'vues/header.php';
+include_once 'models/dataBase.php';
+include_once 'models/users.php';
+include_once 'models/safaryFriend.php';
+include_once 'models/hunts.php';
+include_once 'controllers/usersInfos.php';
+?>
+
+
+<?php
+include 'vues/footer.php';
+?>
+
