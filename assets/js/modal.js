ae4ba3b9f99f948dc05d8b274cebe6b6a932c376
Lexae <atparky@outlook.fr>
2018-11-22 14:11:58 +0100

    Pokedex tout bo

---
+++ assets/js/modal.js
@@ -0,0 +1,24 @@
+// Click function for show the Modal
+
+$(".show").on("click", function(){
+  $(".mask").addClass("active");
+});
+
+// Function for close the Modal
+
+function closeModal(){
+  $(".mask").removeClass("active");
+}
+
+// Call the closeModal function on the clicks/keyboard
+
+$(".close, .mask").on("click", function(){
+  closeModal();
+});
+
+$(document).keyup(function(e) {
+  if (e.keyCode == 27) {
+    closeModal();
+  }
+});
+
